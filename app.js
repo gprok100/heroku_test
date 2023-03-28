1d5ef1f1c01d7d39a5e2eaf450330c8d9725e666
Gyan Prakash <gyanprakashlaishram@gmail.com>
2023-03-28 20:40:59 +0530

    contract add

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ const key = process.env.MY_KEY
 console.log(key)
 const hash = '0xlsh76924nfhsln3ksd8fhsl9shn02h320'
 
+const ERC20_ABI =[
+    // 'function name() view returns(string)',
+    'function symbol() view returns(string)',
+    'function totalSupply() view returns(uint256)',
+    'function balanceOf(address) view returns(uint)'
+]
+
+const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+const contract = new ethers.Contract(address, ERC20_ABI,provider)
+
+let name
+let symbol
+let totalSupply
+let balance
+
+const main =  async() =>{
+     name = await contract.name()
+     symbol = await contract.symbol()
+     totalSupply = await contract.totalSupply()
+     balance = await contract.balanceOf('0x837c20D568Dfcd35E74E5CC0B8030f9Cebe10A28')
+}
+
+
 app.get('/',(req,res)=>{
     res.send(`Heroku testing again and key is ${key}`)
 })
@@ -26,7 +49,13 @@ app.get('/date',(req,res)=>{
 })
 
 app.get('/home',(req,res)=>{
-    res.render('home')
+    main()
+    res.render(`\nReading from address ${address}
+    \n Name: ${name}
+    \nSymbol: ${symbol}
+    \nTotal Supply: ${totalSupply}
+    \nBalance: ${ethers.utils.formatEther(balance)}
+    `)
 })
 
 app.get('/api',(req,res)=>{
